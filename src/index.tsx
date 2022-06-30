fafb5c878b76c84d5653511567ffbbe32dee1442
Fpsska <fpsska1337@gmail.com>
2022-06-30 16:27:09 +0300

    refactor: configure manifest.json, index.html, index.ts for PWA & change logo.png files & add service-worker.js, serviceWorkerRegistration.js files

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
 import { store } from './app/store';
 
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // /. imports
 
@@ -25,5 +25,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
